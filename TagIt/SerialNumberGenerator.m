91777c393565f2f07b1f048526eb0eb113f4ac28
Christopher Olsen <christopher.olsen@target.com>
2016-11-23 13:58:48 -0600

    new files for sn generation and tcin service

---
+++ TagIt/SerialNumberGenerator.m
@@ -0,0 +1,13 @@
+//
+//  SerialNumberGenerator.m
+//  TagIt
+//
+//  Created by Christopher.Olsen on 11/23/16.
+//  Copyright © 2016 Tim.Milne. All rights reserved.
+//
+
+#import "SerialNumberGenerator.h"
+
+@implementation SerialNumberGenerator
+
+@end
---
+++ TagIt/TcinResolverService.h
@@ -0,0 +1,13 @@
+//
+//  TcinResolverService.h
+//  TagIt
+//
+//  Created by Christopher.Olsen on 11/23/16.
+//  Copyright © 2016 Tim.Milne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TcinResolverService : NSObject
+
+@end
